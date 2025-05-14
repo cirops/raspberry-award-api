75465658fddd3619741ae2a0967bf64fbde62a3e
cirops <cirops@gmail.com>
2025-05-14 01:56:25 -0300

    chore: delete unused development-stage route for listing movies

--- src/modules/movie/controllers/movie.controller.ts
+++ src/modules/movie/controllers/movie.controller.ts
@@ -1,16 +1,7 @@
 import { Request, Response } from 'express';
 
 import { MovieRepository } from '../repositories/movie.repository';
-import { GetAllMoviesUseCase } from '../use-cases/get-all-movies.use-case';
 import { GetAwardIntervalsUseCase } from '../use-cases/get-award-intervals.use-case';
-
-export function listMovies(_: Request, res: Response) {
-  const movieRepository = new MovieRepository();
-  const useCase = new GetAllMoviesUseCase(movieRepository);
-  const result = useCase.execute();
-
-  res.json(result);
-}
 
 export function getMovieAwardIntervals(_: Request, res: Response) {
   const movieRepository = new MovieRepository();
--- src/modules/movie/routes/movie.router.ts
+++ src/modules/movie/routes/movie.router.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
 
-import {
-  getMovieAwardIntervals,
-  listMovies,
-} from '../controllers/movie.controller';
+import { getMovieAwardIntervals } from '../controllers/movie.controller';
 
 const movieRouter = Router();
-
-movieRouter.get('/', listMovies);
 
 movieRouter.get('/intervals', getMovieAwardIntervals);
 
