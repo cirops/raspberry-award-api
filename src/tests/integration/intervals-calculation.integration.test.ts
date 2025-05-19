eecaaac242cc32a92db45fb6dfde5ca705d47c69
cirops <cirops@gmail.com>
2025-05-19 10:45:41 -0300

    test(Movie): adjust movie award interval test to verify reference result

--- src/tests/integration/intervals-calculation.integration.test.ts
+++ src/tests/integration/intervals-calculation.integration.test.ts
@@ -3,25 +3,34 @@ import request from 'supertest';
 import { app } from '../../app';
 import { seedDatabase } from '../../shared/database/seed-database';
 
-beforeAll(() => {
-  seedDatabase();
+beforeAll(async () => {
+  await seedDatabase();
 });
 
 describe('GET /movies/intervals', () => {
-  it('should return min and max intervals', async () => {
+  it('should return the expected min and max producer intervals', async () => {
     const response = await request(app).get('/movies/intervals');
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('min');
-    expect(response.body).toHaveProperty('max');
 
-    expect(Array.isArray(response.body.min)).toBe(true);
-    expect(Array.isArray(response.body.max)).toBe(true);
+    const expected = {
+      min: [
+        {
+          producer: 'Joel Silver',
+          interval: 1,
+          previousWin: 1990,
+          followingWin: 1991,
+        },
+      ],
+      max: [
+        {
+          producer: 'Matthew Vaughn',
+          interval: 13,
+          previousWin: 2002,
+          followingWin: 2015,
+        },
+      ],
+    };
 
-    for (const entry of response.body.min) {
-      expect(entry).toHaveProperty('producer');
-      expect(entry).toHaveProperty('interval');
-      expect(entry).toHaveProperty('previousWin');
-      expect(entry).toHaveProperty('followingWin');
-    }
+    expect(response.body).toEqual(expected);
   });
 });
